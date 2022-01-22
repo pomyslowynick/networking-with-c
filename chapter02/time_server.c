fe68c8a5a5194914ec1e7056ea5c856c1f29cfe4
pomyslowynick <rafal.rosa@mycit.ie>
2022-01-22 23:15:53 +0000

    Add connection listener in time server

--- chapter02/time_server.c
+++ chapter02/time_server.c
@@ -6,13 +6,13 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 #else
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #endif
 
 #if defined(_WIN32)
@@ -32,19 +32,55 @@
 
 int main() {
 #if defined(_WIN32)
-    WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d)) {
-        fprintf(stderr, "Failed to initialize. \n");
-        return 1;
-    }
+  WSADATA d;
+  if (WSAStartup(MAKEWORD(2, 2), &d)) {
+    fprintf(stderr, "Failed to initialize. \n");
+    return 1;
+  }
 #endif
-    printf("Configuring local address...\n");
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+  printf("Configuring local address...\n");
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
 
-    struct addrinfo *bind_address;
-    getaddrinfo(0, "8080", &hints, &bind_address);
+  struct addrinfo *bind_address;
+  getaddrinfo(0, "8080", &hints, &bind_address);
+
+  printf("Creating socket...\n");
+  SOCKET socket_listen;
+  socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype,
+                         bind_address->ai_protocol);
+
+  if (!ISVALIDSOCKET(socket_listen)) {
+    fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  printf("Binding socket to local address...\n");
+
+  if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
+    fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  } 
+
+  freeaddrinfo(bind_address);
+
+  printf("Listening...\n");
+  if (listen(socket_listen, 10) < 0) {
+    fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  printf("Waiting for connection...\n");
+  struct sockaddr_storage client_address;
+  socklen_t client_len = sizeof(client_address);
+  SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+
+  if (!ISVALIDSOCKET(socket_client)) {
+    fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  }
+  
 }
